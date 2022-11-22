e87667c3f66bc773e34fd70db34c1a25cd9e3053
sathvik88 <sathvikkonuganti@gmail.com>
2022-11-22 11:57:09 -0500

    Created a new UI for login

--- LeetcodeTracker/Views/MainViews/LoginView.swift
+++ LeetcodeTracker/Views/MainViews/LoginView.swift
@@ -27,21 +27,55 @@ struct LoginView: View {
                     await data.loadStats(name: username)
                 }
         } else{
-
-                VStack {
-                    TextField("Username", text: $username)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .focused($isFocused)
-                    Button("Go") {
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                            login = true
+            NavigationView{
+                ZStack{
+                    Color.cyan
+                        .ignoresSafeArea()
+                    Circle()
+                        .scale(1.7)
+                        .foregroundColor(.white.opacity(0.15))
+                    Circle()
+                        .scale(1.35)
+                        .foregroundColor(.white)
+                    VStack{
+                        Text("Login to Leetcode!")
+                            .font(.largeTitle)
+                            .bold()
+                            .padding()
+                        TextField("Username", text: $username)
+                            .focused($isFocused)
+                            .padding()
+                            .frame(width: 300, height: 50)
+                            .background(Color.black.opacity(0.05))
+                            .cornerRadius(10)
+                        Button("Login"){
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 1){
+                                login = true
+                            }
+                            isFocused = false
                         }
-                        
-                        isFocused = false
-                        
+                        .foregroundColor(.white)
+                        .frame(width: 300, height: 50)
+                        .background(Color.blue)
+                        .cornerRadius(10)
                     }
-      
                 }
+            }
+
+//                VStack {
+//                    TextField("Username", text: $username)
+//                        .textFieldStyle(RoundedBorderTextFieldStyle())
+//                        .focused($isFocused)
+//                    Button("Go") {
+//                        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+//                            login = true
+//                        }
+//
+//                        isFocused = false
+//
+//                    }
+//
+//                }
         }
         
     }
